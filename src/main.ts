335878f090cac2f361e1a28396630fb8fa490967
Stijn van den Berg <489789@student.fontys.nl>
2022-09-06 10:13:25 +0200

    Cleaned up base project files Added TailwindCSS

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import './index.css'
 
 import App from './App.vue'
 import router from './router'
-
-import './assets/main.css'
 
 const app = createApp(App)
 
