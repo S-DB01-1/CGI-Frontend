4ba436da6cdc52a0a916e0392e7c1e320e852f1f
MartijnDerks19 <90613541+MartijnDerks19@users.noreply.github.com>
2022-09-19 14:54:45 +0200

    Added to style

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import Styling from '../views/Styling.vue'
-import devPages from "@/router/devRoutes";
+import devPages from "@/router/devRoutes"
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
