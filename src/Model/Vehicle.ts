eec66264a54f42651e56b043a901876f1fe48a1a
Stijn van den Berg <489789@student.fontys.nl>
2022-09-20 11:54:46 +0200

    Added Vehicle store Converted Vehicle Model to class

--- src/Model/Vehicle.ts
+++ src/Model/Vehicle.ts
@@ -1,5 +1,11 @@
-export type Vehicle = {
+export default class Vehicle {
   ID: number;
   Name: string;
   EmissionPerKM: number;
+
+  constructor(id: number, name: string, emissionPerKM: number) {
+    this.ID = id;
+    this.Name = name;
+    this.EmissionPerKM = emissionPerKM
+  }
 }
---
+++ src/stores/Vehicle.ts
@@ -0,0 +1,64 @@
+// stores/Auth.ts
+import {defineStore} from 'pinia'
+import type Vehicle from "@/Model/Vehicle";
+import type IVehicleDAL from "@/Interface/IVehicleDAL";
+import VehicleDALMemory from "@/DAL/VehicleDALMemory";
+import type {VehicleDTO} from "@/DTO/VehicleDTO";
+
+type VehicleStore = {[key: number]: Vehicle};
+
+let DAL: IVehicleDAL = new VehicleDALMemory();
+
+
+export const useVehicleStore = defineStore('vehicle', {
+  state: () => {
+    return {} as VehicleStore
+  },
+  getters: {
+    Read(state) {
+      return state;
+    },
+    Get(state) {
+      return (id: number) => state[id];
+    },
+  },
+  actions: {
+    async Create(vehicle: Vehicle) {
+      await DAL.Create(vehicle as VehicleDTO).then(vehicle => {
+        this.$state[vehicle.ID] = <Vehicle>vehicle;
+      }).catch(error => {
+        console.error(error);
+      })
+    },
+    async Read() {
+      await DAL.Read().then(vehicles => {
+        vehicles.forEach(vehicle => {
+          this.$state[vehicle.ID] = <Vehicle>vehicle;
+        })
+      }).catch(error => {
+        console.error(error)
+      })
+    },
+    async Get(ID: number) {
+      await DAL.Get(ID).then(vehicle => {
+        this.$state[vehicle.ID] = <Vehicle>vehicle;
+      }).catch(error => {
+        console.error(error)
+      })
+    },
+    async Update(vehicle: Vehicle) {
+      await DAL.Update(vehicle).then(vehicle => {
+        this.$state[vehicle.ID] = <Vehicle>vehicle;
+      }).catch(error => {
+        console.error(error)
+      })
+    },
+    async Delete(ID: number) {
+      await DAL.Delete(ID).then(() => {
+        delete this.$state[ID]
+      }).catch(error => {
+        console.error(error)
+      })
+    }
+  }
+})
