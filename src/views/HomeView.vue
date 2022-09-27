db42ebc761e5d1a1f5d9557e271164eaf7bbc268
MartijnDerks19 <90613541+MartijnDerks19@users.noreply.github.com>
2022-09-27 14:10:42 +0200

    Added general structure home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,32 @@
-<script setup lang="ts">
-import NavigationVue from '@/components/molecules/Navigation.vue'
-</script>
-
 <template>
 <Navigation></Navigation>
+<!-- div winnaar vorig kwartaal  -->
+<div>
+  <h1>Winnaar afgelopen kwartaal!</h1>
+
+
+</div>
+
+<!-- div tekst + afbeelding -->
+<div>
+  <p>Some random text</p>
+
+</div>
+
+<!-- div leaderboardlijst -->
+<div>
+
+</div>
+<!-- div grafiek + tekst -->
+<div>
+<p></p>
+</div>
 </template>
+
+<script setup lang="ts">
+  import Navigation from '../../components/molecules/Navigation.vue'
+</script>
+
+<style>
+
+</style>
