2dc1c5639aedc2001bdd4caa90ab13cf3a6699d2
Tom <54415846+BuddieDJ@users.noreply.github.com>
2022-10-18 09:53:05 +0200

    Kwam nog een mobiele foutje tegen, deze heb ik opgelost

--- src/views/admin/vehicles/Vehicles.vue
+++ src/views/admin/vehicles/Vehicles.vue
@@ -3,28 +3,30 @@
     <Navigation type="admin"></Navigation>
     <div class="bg-background h-screen">
       <Section>
-        <div class="w-100 mx-6 bg-white lg:w-2/3 lg:p-0 m-auto shadow-md rounded-lg mb-3">
-          <Title size="4" color="primary" weight="bold" class="mb-6">
-            View all vehicles
-          </Title>
+        <div class="mx-6 lg:m-0">
+          <div class="w-100 lg:w-2/3 lg:p-0 m-auto rounded-lg mb-3">
+            <Title size="4" color="primary" weight="bold" class="mb-6">
+              View all vehicles
+            </Title>
 
-          <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
-            <Table :items="items" :fields="fields">
-              <template #cell(title)="{ value, item }">
-                <p><strong>{{ value }}</strong></p>
-                <p>{{ item.id }}</p>
-              </template>
-            </Table>
-          </div>
+            <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+              <Table :items="items" :fields="fields">
+                <template #cell(title)="{ value, item }">
+                  <p><strong>{{ value }}</strong></p>
+                  <p>{{ item.id }}</p>
+                </template>
+              </Table>
+            </div>
 
-          <div class="flex justify-end py-3">
-            <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-              Previous
-            </a>
+            <div class="flex justify-end py-3">
+              <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+                Previous
+              </a>
 
-            <a href="#" class="inline-flex items-center py-2 px-4 ml-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
-              Next
-            </a>
+              <a href="#" class="inline-flex items-center py-2 px-4 ml-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700">
+                Next
+              </a>
+            </div>
           </div>
         </div>
       </Section>
