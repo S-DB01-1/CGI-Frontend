5eff387ebb818d808c2dbd507a833067b906ec73
BuddieDJ <dj.tommaessen04@gmail.com>
2023-01-13 10:45:30 +0100

    Basic versie voor de authentication gemaakt.

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,6 +6,7 @@ import adminRoutes from "@/router/adminRoutes"
 import CreateTrip from '../views/Trip/CreateTrip.vue'
 import UserView from '../views/User/UserView.vue'
 import User from '@/Model/User';
+import Login from '../views/Auth/Login.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,30 +15,59 @@ const router = createRouter({
       path: '/',
       name: 'home',
       alias: '/home',
+      meta: {
+        requiresAuth: true,
+      },
       component: CreateTrip
     },
-    {
-      path: '/styling',
-      name: 'styling',
-      component: Styling
-    },
+    // {
+    //   path: '/styling',
+    //   name: 'styling',
+    //   component: Styling
+    // },
     {
       path: '/user',
       name: 'user',
+      meta: {
+        requiresAuth: true,
+      },
       component: UserView
     },
-
+    {
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
     ...devPages,
     ...adminRoutes,
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
+checkState()
+function checkState() {
+  let state = false;
+  const response = fetch("http://127.0.0.1:5000/api/user/state")
+      .then(response => {console.log(response); return response})
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        state = data.state
+      })
+  console.log(state)
+  return state
+}
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requiresAuth)) {
+    if (checkState()) {
+      next();
+    } else {
+      next({
+        path: "/login",
+      });
+    }
+  } else {
+    next();
+  }
+});
 
 export default router
---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,71 @@
+<template>
+  <Navigation type="user"></Navigation>
+  <div>
+    <Hero>
+      <Title size="1" weight="bold" color="primary">
+        Add new trip
+      </Title>
+    </Hero>
+    <Section>
+      <div class="container mx-auto">
+        <div class="flex flex-col md:flex-row gap-4">
+          <div class="w-full lg:w-3/5 py-4 px-6 m-auto">
+            <Button v-on:click="login()">Login</Button>
+          </div>
+        </div>
+      </div>
+    </Section>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import Section from '../../components/molecules/Section.vue'
+import Button from '../../components/atoms/Forms/Button.vue'
+import Title from '../../components/atoms/Title.vue'
+import Hero from '../../components/molecules/Hero.vue'
+import Navigation from '../../components/molecules/Navigation.vue'
+
+
+function login() {
+  window.location.href = "http://127.0.0.1:5000/api/user/login";
+}
+
+</script>
+
+<style scoped>
+ul li:last-child {
+  border-bottom: none;
+}
+
+.trip-segment-container > ul {
+  border: 0 none;
+  margin: 0;
+  width: 100%;
+  padding-left: 40px;
+  position: relative;
+}
+
+.trip-segment-container > ul::before {
+  content: "";
+  display: inline-block;
+  width: 8px;
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+  background: -webkit-gradient(linear,left bottom,left top,color-stop(0,#5236ab),to(#e41937));
+  background: linear-gradient(360deg,#5236ab 0,#e41937 100%);
+}
+
+.backdrop {
+  background-color: rgba(0,0,0,0.5);
+}
+
+@media (min-width: 640px) {
+  .container {
+    max-width: 100% !important;
+  }
+}
+</style>
